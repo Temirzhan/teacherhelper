f806dd83f7f2a9bb6ad075ea075f0f346933e93a
repen <9keepa@gmail.com>
2020-10-17 22:45:10 +0500

    config updated

--- app/Globals.py
+++ app/Globals.py
@@ -1,8 +1,8 @@
 import os
 
 PATH = os.getenv( "APP_PATH", os.getcwd() )
-SENDER_EMAIL  = os.getenv( "SENDER_EMAIL",  "repente1@yandex.ru" )
+SENDER_EMAIL  = os.getenv( "SENDER_EMAIL",  "" )
 SMTP_SERVER   = "smtp.yandex.com"
-smtp_user     = os.getenv( "SMTP_USER",  "repente1" )
-smtp_passwd   = os.getenv( "SMTP_PASSW", "password" )
+smtp_user     = os.getenv( "SMTP_USER",  "" )
+smtp_passwd   = os.getenv( "SMTP_PASSW", "" )
 UPLOAD_FOLDER = os.path.join(PATH, "data", "report_storage")
